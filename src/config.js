f381bc005af9c1245f481dfb22ddb120919cc0ff
MytsV <mytsvictoria@gmail.com>
2023-06-02 21:59:52 +0300

    Implement configuration handling
    Initiate the possibility to dynamically get app's configuration.
    Currently, configuration handler supports only PATH entry, which points to database directory.
    The config is user specific, hence os.homedir() calls.
    If no config is found in the user's home directory, it is automatically created with default value.

---
+++ src/config.js
@@ -0,0 +1,44 @@
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+
+const configName = '.todolor.conf';
+const defaultPath = '.todolor';
+
+// TODO: Write a small util to paint text
+const yellowAnsi = '\x1b[33m';
+const defaultAnsi = '\x1b[39m';
+
+const parse = (contents) => {
+  // TODO: Rewrite without regexp
+  const regExp = /PATH=(.*)/;
+  const result = regExp.exec(contents);
+  if (!result || result.length === 0) {
+    throw Error('Corrupted configuration file');
+  } else {
+    return result[0];
+  }
+};
+
+const createConfig = (configPath) => {
+  const databasePath = path.join(os.homedir(), defaultPath);
+  const entry = `PATH=${databasePath}`;
+  fs.writeFileSync(configPath, entry);
+  return databasePath;
+};
+
+const getDatabasePath = () => {
+  const configPath = path.join(os.homedir(), configName);
+  let contents;
+  try {
+    contents = fs.readFileSync(configPath);
+  } catch (e) {
+    console.log(`${yellowAnsi}${configPath} not found${defaultAnsi}`);
+    const result = createConfig(configPath);
+    console.log(`${yellowAnsi}Initialized ${configPath}${defaultAnsi}`);
+    return result;
+  }
+  return parse(contents);
+};
+
+module.exports = {getDatabasePath};
