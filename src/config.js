7511df0471f60193d679f0d57fc2a0f1509cf377
MytsV <mytsvictoria@gmail.com>
2023-06-03 01:49:41 +0300

    Rewrite parse function of config handler without RegExp
    Additionally, document the function

--- src/config.js
+++ src/config.js
@@ -9,15 +9,39 @@ const defaultPath = '.todolor';
 const yellowAnsi = '\x1b[33m';
 const defaultAnsi = '\x1b[39m';
 
-const parse = (contents) => {
-  // TODO: Rewrite without regexp
-  const regExp = /PATH=(.*)/;
-  const result = regExp.exec(contents);
-  if (!result || result.length === 0) {
-    throw Error('Corrupted configuration file');
-  } else {
-    return result[1];
+// Keys which parser allows to be present in configuration file
+const allowedKeys = ['path', 'debug'];
+
+/*
+Returns object containing all the key-value pairs from a configuration string.
+The format is as follows:
+- content can't be empty
+- every non-empty line is a valid key separated by '=' from corresponding value
+ */
+const parse = (content) => {
+  const errorMsg = 'Corrupted configuration file';
+  // Discards empty configuration
+  if (content.length === 0) throw Error(errorMsg);
+
+  const result = {};
+
+  const lines = content.split('\n');
+  for (const line of lines) {
+    // Skips empty lines
+    if (line.length === 0) continue;
+
+    const idx = line.indexOf('=');
+    if (idx < 0) {
+      throw Error(errorMsg);
+    }
+    const [key, value] = [line.slice(0, idx), line.slice(idx +1)];
+    if (!allowedKeys.includes(key.toLowerCase())) {
+      throw Error(errorMsg);
+    }
+    result[key.toLowerCase()] = value;
   }
+
+  return result;
 };
 
 const createConfig = (configPath) => {
@@ -29,16 +53,16 @@ const createConfig = (configPath) => {
 
 const readConfig = () => {
   const configPath = path.join(os.homedir(), configName);
-  let contents;
+  let content;
   try {
-    contents = fs.readFileSync(configPath);
+    content = fs.readFileSync(configPath);
   } catch (e) {
     console.log(`${yellowAnsi}${configPath} not found${defaultAnsi}`);
     const result = createConfig(configPath);
     console.log(`${yellowAnsi}Initialized ${configPath}${defaultAnsi}`);
     return {path: result};
   }
-  return {path: parse(contents)};
+  return parse(content.toString());
 };
 
 module.exports = {readConfig};
