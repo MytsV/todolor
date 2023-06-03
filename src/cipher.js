8a514416de26b574061e6f38dab15cf790d39eb8
MytsV <mytsvictoria@gmail.com>
2023-06-03 14:26:46 +0300

    Implement encoding via Vignere cipher variation
    Caesar-like, but shifts are dynamic and dependent on position in a key.

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,24 @@
+// Doesn't care about what these bytes represent.
+// Implemented not for data security, but protection from easy manipulation.
+
+const byteSize = 256;
+const key = 'todolor';
+
+const rotate = (bytes, mul = 1) => {
+  const result = Buffer.from(bytes);
+  for (let i = 1; i <= bytes.length; i++) {
+    const shift = key.charCodeAt(i % key.length);
+    result[i - 1] = (bytes[i - 1] + shift * mul) % byteSize;
+  }
+  return result;
+};
+
 const encode = (bytes) => {
-  return bytes;
+  return rotate(bytes);
 };
 
 const decode = (bytes) => {
-  return bytes;
+  return rotate(bytes, -1);
 };
 
 module.exports = {encode, decode};
